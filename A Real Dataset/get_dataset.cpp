e6ce4446310b05c9a0a9a22710fcda4cd16123fb
SiddheshKotwal <siddheshkotwal379@gmail.com>
2024-07-17 14:02:42 +0530

    made a cmake files for opencv setup

--- A Real Dataset/get_dataset.cpp
+++ A Real Dataset/get_dataset.cpp
@@ -1,19 +1,15 @@
-// #include "F:\Codes\Neural Networks From Scratch\Github Repo\Neural-Networks-From-Scratch-in-CPP\common_includes.h"
-// #include <opencv2/opencv.hpp>
-// using namespace cv;
+#include "F:\Codes\Neural Networks From Scratch\Github Repo\Neural-Networks-From-Scratch-in-CPP\common_includes.h"
+#include <opencv2/opencv.hpp>
 
-// int main(){
+int main() {
 
-//     Mat image_data = imread("fashion_mnist_images/train/7/0002.png", IMREAD_UNCHANGED);
-
-//     if (image_data.empty()) {
-//         cout << "Could not open or find the image" << endl;
-//         return -1;
-//     }
-
-//     namedWindow("Display Image", WINDOW_AUTOSIZE);
-//     imshow("Display Image", image_data);
-//     waitKey(0);
-
-//     return 0;
-// }
+    cv::Mat image = cv::imread("F:/Codes/Neural Networks From Scratch/Github Repo/A Real Dataset/fashion_mnist_images/train/7/0002.png", cv::IMREAD_UNCHANGED); // Load an image
+    if (image.empty()) {
+        std::cerr << "Could not open or find the image!" << std::endl;
+        return -1;
+    }
+    // cout<<image<<" \n";
+    cv::imshow("Display window", image); // Show the image in a window
+    cv::waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
